0276e2ff5bd849afdf9a9abbc0530d08fcca5a24
Hanna Shymanovych <shimsnov111@gmail.com>
2025-02-19 17:51:05 +0100

    Search - done

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,22 @@
-let searchRequest = 'frozen'
+const filmSearchInput = document.getElementById("film-search")
+const searchBtn = document.getElementById("search-btn")
+const searchResultsContainer = document.getElementById("search-results-container")
 
 
-async function handleSearch() {
+
+searchBtn.addEventListener('click', ()=> {
+    const userInput = filmSearchInput.value
+    if(userInput){
+        handleSearch(userInput);
+    }
+    
+} )
+
+
+
+
+
+async function handleSearch(searchRequest) {
     const apiKey = "976c63cc"; // Your API key
     const searchUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchRequest}`;
 
@@ -22,6 +37,43 @@ async function handleSearch() {
             // Step 3: Wait for all requests to complete
             const movieDetails = await Promise.all(movieDetailsPromises);
 
+            let html = ``
+
+
+            movieDetails.forEach(movie => {
+                html += `
+                <div id="movie-id" class="movie">
+                <img id="movie-img" class="movie-img" src="${movie.Poster}" alt="">
+                <div id="movie-info" class="movie-info">
+                    <div id="movie-title-container" class="movie-title-container">
+                        <h2 id="movie-title" class="movie-title">${movie.Title}</h2>
+                        <i class="fa-solid fa-star movie-star-icon"></i>
+                        <p id="movie-raiting" class="movie-raiting">${movie.imdbRating}</p>
+                    </div>
+                    <div class="movie-subtitle-container">
+                        <p class="movie-duration" id="movie-duration">${movie.Runtime}</p>
+                        <p id="movie-genre" class="movie-genre">${movie.Genre}</p>
+                        <button id="add-to-watchlist-btn" class="add-to-watchlist-btn"><span><i class="fa-solid fa-circle-plus add-to-my-watchlist-icon"></i></span> Watchlist</button>
+                    </div>
+                    
+                    <p id="movie-plot" class="movie-plot">${movie.Plot}</p>
+                    
+                </div>
+                
+            </div>
+            <div id="movie-separator" class="movie-separator"></div>
+                `
+              
+            })
+
+           
+            
+
+            searchResultsContainer.innerHTML= html
+
+
+            
+
             console.log(movieDetails); // Full details of all movies
         } else {
             console.error("No movies found:", data.Error);
@@ -31,4 +83,11 @@ async function handleSearch() {
     }
 }
 
-handleSearch();
+
+
+//poster
+//movie title
+//rating
+//duration
+//genre
+//plot
